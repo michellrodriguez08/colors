fd3449889af9bd364090b12f63323b6993ad81aa
Michell Rodriguez <rodriguez.michell@student.greenriver.edu>
2023-01-12 00:37:14 -0500

    Create color form

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pick a Color</title>
+    <form method="post" action="confirm.php">
+        <label><input type="radio" value="red" name="color">Red</label><br>
+        <label><input type="radio" value="blue" name="color">Blue</label><br>
+        <label><input type="radio" value="yellow" name="color">Yellow</label><br>
+        <input type="submit" value="Submit">
+
+    </form>
+
 </head>
 <body>
     <h1>Pick a color</h1>
